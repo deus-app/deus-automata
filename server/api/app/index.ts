a8cceaab93e2e8dde432abe04eb1442f0c08a3bf
chanon-mike <zzmikezz8520@gmail.com>
2023-12-31 22:51:38 +0900

    chore: install husky for pre-commit

--- server/api/app/index.ts
+++ server/api/app/index.ts
@@ -1,5 +1,5 @@
-import type { VisionModel } from '$/commonTypesWithClient/models';
 import type { DefineMethods } from 'aspida';
+import type { VisionModel } from 'commonTypesWithClient/models';
 
 export type Methods = DefineMethods<{
   // TODO: Change body type
